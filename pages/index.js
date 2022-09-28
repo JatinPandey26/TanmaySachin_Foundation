4d2a07e6dc491000e981eaed5939ee682c1633c4
Jatin <jatin26pandey@gmail.com>
2022-09-28 11:18:52 +0530

    check

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! git integration Test by Jatin new branch new laptop</a>
+          Welcome to <a href="https://nextjs.org">Next.js! git integration Test by Jatin new branch new laptop new check</a>
         </h1>
 
         <p className={styles.description}>
