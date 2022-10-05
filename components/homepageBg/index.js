450c896fe0a9f5dfa378d0f0ead59a34db94b1cc
JatinPandey <jatin26pandey@gmail.com>
2022-10-05 12:33:49 +0530

    homepage bg update

--- components/homepageBg/index.js
+++ components/homepageBg/index.js
@@ -2,8 +2,12 @@ import React from 'react'
 import styles from './styles.module.scss'
 export const HomePic = () => {
   return (
-    <div className={styles.wrapper} style = {{backgroundImage : `url('/homeMainbg.jpg')`}}>
-        <h2>“ Unlimited <span>food</span> for <span>Hunger</span> ”</h2>
+    <div className={styles.wrapper} >
+      <div className={styles.headings}>
+        <h2> Endless <span>food</span> for <span>Hunger</span> </h2>
+        <h3 >Help Us to Feed those in Need </h3>
+        <button className='font-semibold'>Support Us</button>
+        </div>
     </div>
   )
 }
