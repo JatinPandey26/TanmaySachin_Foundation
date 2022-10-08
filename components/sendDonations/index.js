d72c9dac9949012bf438efd63ceb5f6b0f9b5a56
JatinPandey <jatin26pandey@gmail.com>
2022-10-08 17:33:55 +0530

    Home page

--- components/sendDonations/index.js
+++ components/sendDonations/index.js
@@ -1,15 +1,18 @@
+import Link from "next/link";
 import React from "react";
 import styles from "./styles.module.scss";
 
 const SendDonationBand = () => {
   return (
     <div className={styles.container}>
-      
-        <div className={styles.leftContainer}>
-          <img src="/donateCircle.jpg"></img>
-        </div>
-        <div className={styles.rightContainer}>
-          <h1>Send Us Your Donation</h1>
+      <div className={styles.leftContainer}>
+        <h1>Send Us Your Donation</h1>
+        <button>
+          <Link href={"/donate"}>Donate Now</Link>
+        </button>
+      </div>
+      <div className={styles.rightContainer}>
+        <div className="p-3 m-2 ">
           <p>
             Every donation will lead to a great future & Inspire many more
             people.No matter what you serve, what you donate what you give,each
@@ -19,10 +22,30 @@ const SendDonationBand = () => {
               80g TAX BENEFITS
             </span>
           </p>
-          <button>Donate Now</button>
+        </div>
+        <div className=" p-3 m-2">
+          <p>
+            Every donation will lead to a great future & Inspire many more
+            people.No matter what you serve, what you donate what you give,each
+            & every donation will make great impact & spread happiness. Donate
+            and get{" "}
+            <span className="font-semibold text-[#F01152] italic">
+              80g TAX BENEFITS
+            </span>
+          </p>
+        </div>
+        <div className=" p-3 m-2">
+          <p>
+            Every donation will lead to a great future & Inspire many more
+            people.No matter what you serve, what you donate what you give,each
+            & every donation will make great impact & spread happiness. Donate
+            and get{" "}
+            <span className="font-semibold text-[#F01152] italic">
+              80g TAX BENEFITS
+            </span>
+          </p>
+        </div>
       </div>
-
-      
     </div>
   );
 };
