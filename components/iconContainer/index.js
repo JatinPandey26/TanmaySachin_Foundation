ed95874e8e84a0fef4c4413813fd6d182b2f6326
JatinPandey <jatin26pandey@gmail.com>
2022-10-08 19:15:31 +0530

    home page new section

---
+++ components/iconContainer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.scss";
+const IconContainer = ({img , text}) => {
+  return (
+    <div className={styles.wrapper}>
+      <div><img src={img}></img></div>
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default IconContainer;
