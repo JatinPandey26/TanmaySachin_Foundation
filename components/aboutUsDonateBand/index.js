536e64c24b8e4ebf18bfc6bb53b7204f58d67f4e
Aditi Gemini <aditi152003@gmail.com>
2022-10-09 16:41:05 +0530

    aboutUs

--- components/aboutUsDonateBand/index.js
+++ components/aboutUsDonateBand/index.js
@@ -6,7 +6,7 @@ export const DonateBand = () => {
   return (
     <div className={styles.donateBandWrap}>
       <div className="text-3xl text-white w-[40vw] ">
-        Let's make a difference in lives of others
+        Lets make a difference in lives of others
       </div>
       <div className={styles.buttons}>
         <button className="font-extralight">
