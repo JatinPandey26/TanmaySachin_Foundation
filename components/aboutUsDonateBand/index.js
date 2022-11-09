09db371ce444e4d83389b1b8ccc1e425601fd298
JatinPandey <jatin26pandey@gmail.com>
2022-11-09 11:28:20 +0530

    fixes and backend

--- components/aboutUsDonateBand/index.js
+++ components/aboutUsDonateBand/index.js
@@ -5,15 +5,19 @@ import Link from "next/link";
 export const DonateBand = () => {
   return (
     <div className={styles.donateBandWrap}>
-      <div><img src="/images.jpeg"></img></div>
-      <div className={styles.donateText}>
-      <div className="text-[2.5rem] text-white w-[45vw] ">
-         Make a difference in lives of others!
+      <div>
+        <img src="/images.jpeg" ></img>
       </div>
-      <div className={styles.buttons}>
-        <button className="font-extralight">
-          <Link href={"https://rzp.io/l/cLPVuqwKtx"}>Help Us By Donating</Link>
-        </button>
+      <div className={styles.donateText}>
+        <div className="text-[2.5rem] text-white w-[60% ] ">
+          Make a difference in lives of others!
+        </div>
+        <div className={styles.buttons}>
+          <button className="font-extralight">
+            <Link href={"https://rzp.io/l/cLPVuqwKtx"}>
+              Help Us By Donating
+            </Link>
+          </button>
         </div>
       </div>
     </div>
--- components/navbar/index.js
+++ components/navbar/index.js
@@ -22,7 +22,7 @@ function Navbar() {
                   height="70%"
                 />
                 <div className={styles.title}>
-                  <p className="self-start w-10 font-extrabold ml-2 text-[#152453]">
+                  <p className="self-start w-28 font-extrabold ml-2 text-[#152453]">
                     Tanmay Sachin Foundation
                   </p>
                 </div>
