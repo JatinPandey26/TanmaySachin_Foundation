a489b2448170dab1f10c3936c048be7e25d375b3
JatinPandey <jatin26pandey@gmail.com>
2022-11-13 22:15:47 +0530

    home page NEW TEST 2

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,7 +1,6 @@
 
-
 const mail = require("@sendgrid/mail");
-// mail.setApiKey(process.env.NEXT_PUBLIC_MAIL_API_KEY);
+mail.setApiKey(process.env.NEXT_PUBLIC_MAIL_API_KEY);
 export default function handler(req, res) {
   const data = JSON.parse(req.body);
   console.log(data);
@@ -11,15 +10,15 @@ export default function handler(req, res) {
   const message =
     "You are the special type of person that changes lives, lifts people up, and makes the world a better place. Thank you for your donation and your association with our cause.";
 
-  // const details = {
-  //   from: "jatin26pandey@gmail.com", // sender address
-  //   to: userEmail, // list of receivers
-  //   subject: `Thankyou for your suppport ${userName}`,
-  //   text: message,
-  // };
-  // mail.send(details).then(() => {
-  //   console.log("Success");
-  // });
+  const details = {
+    from: "jatin26pandey@gmail.com", // sender address
+    to: userEmail, // list of receivers
+    subject: `Thankyou for your suppport ${userName}`,
+    text: message,
+  };
+  mail.send(details).then(() => {
+    console.log("Success");
+  });
   console.log("HELLO");
   res.status(200).json({ message: "Success" });
 }
